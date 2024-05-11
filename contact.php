677408482beec81af3579018986592fed76fb99e
Aaryannnnnn <superbaryanaz@gmail.com>
2024-05-11 17:43:07 +0530

    Airline reservation

---
+++ contact.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<?php
+include "header.php";
+?>
+</head>
+<body>
+<?php    include "top.php" ?>
+<div align="center">
+<br/><br/>
+<h2>Contacts</h2>
+<br/>
+<br/>For Payment Pay to 
+<br/><font color= 'magenta',  size ='5'>SBI Account number 0000000000
+<br/>ICICI Account number 1111111
+<hr/><font color='purple'>
+Contact Aryan singh superbaryanaz@gmail.com
+</font color>
+
+</div>
+<?php  include "bottom.php "?>
+</body>
+</html>
---
+++ our-aircraft.php
@@ -0,0 +1,62 @@
+<html>
+<head>
+<?php
+include "header.php";
+?>
+</head>
+<body>
+<?php    include "top.php" ?>
+<div align = center>
+
+<h2>Our AirCraft </h2>
+<br/>
+<p><br/><b>Boeing 747-400: 747/ 744/ 74R</b>
+<br/><b>No. of aircraft</b>   6/ 4/ 6
+No. of seats      389/ 375/ 375
+<br/><b>Class of travel</b>  Royal First Class:14/ 10/ 10, Royal Silk Class:50/ 40/ 40, Economy Class:325/ 325/ 325	
+<br/>
+<br/><b>Airbus 340-600</b>
+<br/><b>No. of aircraft</b>   6
+<br/><b>No. of seats</b>      267
+<br/><b>Class of travel</b>  Royal First Class:8, Royal Silk Class:60, Economy Class:199
+<br/>
+<br/><b>Airbus 340-500</b>
+<br/><b>No. of aircraft</b>    4
+<br/>No. of seats      215
+<br/><b>Class  of travel</b>  Royal Silk Class:60, Premium Economy Class:42, Economy Class: 113
+<br/>	
+<br/><b>Boeing 777-300</b>
+<br/><b>No. of aircraft  6</b>
+<br/><b>No. of seats</b>      364
+<br/><b>Class of travel</b>  Royal Silk Class:34, Economy Class: 330	
+<br/>
+<br/><b>Boeing 777-300ER</b>
+<br/><b>No. of aircraft</b>   5
+<br/><b>No. of seats</b>      312
+<br/><b>Class of travel</b>  Royal First Class:8, Royal Silk Class:30, Economy Class: 274
+<br/>
+<br/><b>Boeing 777-200</b>
+<br/></b>No. of aircraft</b>   8
+<br/><b>No. of seats</b>      309
+<br/><b>Class of travel</b>  Royal Silk Class:30, Economy Class: 279
+<br/>	
+<br/><b>Boeing 777-200ER</b>
+<br/><b>No. of aircraft</b>   6
+<br/><b>No. of seats</b>      292
+<br/><b>Class of travel</b>  Royal Silk Class:30, Economy Class: 262
+<br/>	
+<br/><b>Airbus 330-300: 333/ 330/ 33H<b>
+<br/><b>No. of aircraft </b>  12/ 8/ 3
+<br/><b>No. of seats </b>     305/ 299/ 299
+<br/><b>Class of travel</b>  Royal Silk Class:42/ 36/ 36, Economy Class: 263/ 263/ 263
+<br/>
+<br/><b>Airbus 300-600: AB6/ AB7</b>
+<br/><b>No. of aircraft </b>  6/ 5
+<br/><b>No. of seats</b>      247/ 260
+<br/><b>Class of travel</b>  Royal Silk Class:46/ 28, Economy Class: 201/ 232</p>
+
+
+</div>
+<?php  include "bottom.php "?>
+</body>
+</html>
